c5113a1893316906ded1e6bf88128fc8b6dbda35
hiepnguyen57 <hoahiepk10@gmail.com>
2022-04-02 16:43:18 +0700

    use new styles

--- include/Log.h
+++ include/Log.h
@@ -1,135 +1,130 @@
+#pragma once
+
 #include <sstream>
 #include <string>
 #include <mutex>
 #include <iomanip>
+#include <memory>
+
 #include "Level.h"
 
-//#define NDEBUG
-#ifdef NDEBUG
-#define LOG_DEBUG     Log<OutputToFile>().Print(Level::NONE)
-#define LOG_INFO      Log<OutputToFile>().Print(Level::NONE)
-#define LOG_WARN      Log<OutputToFile>().Print(Level::NONE)
-#define LOG_ERROR     Log<OutputToFile>().Print(Level::NONE)    
-#define LOG_CRITICAL  Log<OutputToFile>().Print(Level::NONE)    
+#define LOG_TAG "Logger"
+
+#ifdef NDEBUG 
+#define LOG_DEBUG(...) do{} while(0)
+#define LOG_INFO(...) do{} while(0)
+#define LOG_WARN(...) do{} while(0)
+#define LOG_ERROR(...) do{} while(0)   
+#define LOG_CRITICAL(...) do{} while(0)  
 #else //DEBUG
 
-#define LOG_DEBUG     Log<OutputToFile>().Print(Level::DEBUG)
-#define LOG_INFO      Log<OutputToFile>().Print(Level::INFO)
-#define LOG_WARN      Log<OutputToFile>().Print(Level::WARN)
-#define LOG_ERROR     Log<OutputToFile>().Print(Level::ERROR)
-#define LOG_CRITICAL  Log<OutputToFile>().Print(Level::CRITICAL)
+#define LOG_DEBUG(...) Log<OutputToFile>().write(convertLevelToName(Level::DEBUG), LOG_TAG, __VA_ARGS__)
+#define LOG_INFO(...) Log<OutputToFile>().write(convertLevelToName(Level::INFO), LOG_TAG, __VA_ARGS__)
+#define LOG_WARN(...) Log<OutputToFile>().write(convertLevelToName(Level::WARN), LOG_TAG, __VA_ARGS__)
+#define LOG_ERROR(...) Log<OutputToFile>().write(convertLevelToName(Level::ERROR), LOG_TAG, __VA_ARGS__)
+#define LOG_CRITICAL(...) Log<OutputToFile>().write(convertLevelToName(Level::CRITICAL), LOG_TAG, __VA_ARGS__)
 #endif
-
 
 /*
  * Log class.
- * typename log_policy is output policy: stderr, stdout, OutputToFile, etc.
+ * typename LogPolicy is output policy: stderr, stdout, OutputToFile, etc.
  */
-template <typename log_policy>
+template <typename LogPolicy>
 class Log {
 public:
     Log();
     virtual ~Log();
-    std::ostringstream& Print(Level level = Level::INFO);
     Level& getCurrentLevel();
     void setLogLevel(Level newLevel);
+
+    template <typename TF, typename... TR>
+    void write(TF const& f, TR const&... rest);
+
+    template <typename TF>
+    void write(TF const& f);
+
+    void write();
+
 protected:
     std::ostringstream os;
+
 private:
-    inline const tm* getLocalTime();
+    template<typename TF>
+    inline void writeOutput(TF const& f);
+
 private:
-    std::mutex    m_oMutex;
+    std::mutex mMutex;
     Log(const Log&);
-    Log& operator =(const Log&);
-    Level m_level;
+    Log& operator=(const Log&);
+    Level mLevel;
     tm mLocalTime;
 };
 
-template <typename log_policy> 
-Log<log_policy>::Log()
-{
+template <typename LogPolicy>
+Log<LogPolicy>::Log() {}
+
+template <typename LogPolicy> 
+template <typename TF, typename... TR>
+void Log<LogPolicy>::write(TF const& f, TR const&... rest) {
+    writeOutput(f);
+    os << " ";
+    write(rest...);
 }
 
-template <typename log_policy> 
-std::ostringstream& Log<log_policy>::Print(Level level)
-{
-    m_oMutex.lock();
-    if(level != Level::NONE)
-    {
-        os << "[" << std::put_time(getLocalTime(), "%Y-%m-%d %H:%M:%S") << "]";   
-        os << "[" << convertLevelToName(level) << "]\t";    
-    }
-    m_level = level;
-    m_oMutex.unlock();
-    return os;
+template <typename LogPolicy> 
+template <typename TF>
+void Log<LogPolicy>::write(TF const& f) {
+    writeOutput(f);
 }
+
+template <typename LogPolicy> 
+void Log<LogPolicy>::write() {
+    // handle the empty params case
+}
+
 /*
  * In the destructor print out the message
  */
-template <typename log_policy> 
-Log<log_policy>::~Log()
-{
-    if(m_level != Level::NONE) {
+template <typename LogPolicy>
+Log<LogPolicy>::~Log() {
+    if (mLevel != Level::NONE) {
         os << std::endl;
-        log_policy::Output(os.str());   
-    } 
-
+        LogPolicy::Output(os.str());
+    }
 }
 
-template <typename log_policy> 
-Level& Log<log_policy>::getCurrentLevel()
-{
-    return m_level;
+template <typename LogPolicy>
+Level& Log<LogPolicy>::getCurrentLevel() {
+    return mLevel;
 }
 
-template <typename log_policy>
-void Log<log_policy>::setLogLevel(Level newLevel)
-{
-    m_level = newLevel;
+template <typename LogPolicy>
+void Log<LogPolicy>::setLogLevel(Level newLevel) {
+    mLevel = newLevel;
 }
 
-template <typename log_policy> 
-inline const tm* Log<log_policy>::getLocalTime() {
-    auto in_time_t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    localtime_r(&in_time_t, &mLocalTime);
-    return &mLocalTime;
+template <typename LogPolicy>
+template <typename TF>
+inline void Log<LogPolicy>::writeOutput(TF const& f) {
+    std::lock_guard<std::mutex> lock(mMutex);
+    os << f;
 }
-
 
 class OutputToFile {
-public:
+   public:
     static FILE*& Stream();
     static void Output(const std::string& msg);
 };
 
-inline FILE*& OutputToFile::Stream()
-{
+inline FILE*& OutputToFile::Stream() {
     static FILE* pStream = stderr;
     return pStream;
 }
 
-inline void OutputToFile::Output(const std::string& msg)
-{
+inline void OutputToFile::Output(const std::string& msg) {
     FILE* pStream = Stream();
     if (!pStream) return;
 
     fprintf(pStream, "%s", msg.c_str());
     fflush(pStream);
 }
-
-
-// #include <sys/time.h>
-// inline std::string LogNowTime()
-// {
-//  char buffer[64];
-//  time_t t;
-//  time(&t);
-//  tm r = {0};
-//  strftime(buffer, sizeof(buffer), "%Y-%m-%d  %H:%M:%S", localtime_r(&t, &r));
-//  struct timeval tv;
-//  gettimeofday(&tv, 0);
-//  char result[100] = {0};
-//  //std::sprintf(result, "%s.%03ld", buffer, (long)tv.tv_usec / 1000); 
-//  sprintf(result, "%s.%06ld", buffer, (long)tv.tv_usec / 1000); 
-//  return result;
-// }
--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,41 @@
 
 using namespace std;
 
-void threadFunc(int id)
-{
-	for (int i = 0; i < 100; i++)
-	{
-	    // add a log message
-		LOG_DEBUG << "Thread " << id << ", Message " << i;
-	}
-}
-
-int main()
-{
-#ifdef FILE_LOGGER
-	FILE* pFile = fopen("application.log", "a");
-	OutputToFile::Stream() = pFile;
+#ifdef LOG_TAG
+#undef LOG_TAG
+#define LOG_TAG "MAIN"
 #endif
 
-	list<shared_ptr<thread>> oThreads;
+void threadFunc(int id) {
+    for (int i = 0; i < 100; i++) {
+        // add a log message
+        LOG_DEBUG("Thread", id, "Message", i);
+    }
+}
 
-	// create 100 threads
-	for (int i = 0; i < 100; i++)
-	{
-		oThreads.push_back(shared_ptr<thread>(new thread(threadFunc, i)));
-	}
+int main() {
+#ifdef FILE_LOGGER
+    FILE* pFile = fopen("application.log", "a");
+    OutputToFile::Stream() = pFile;
+#endif
 
-	// also do logging from the main thread
-	for (int i = 0; i < 100; i++)
-	{
-	    // add a log message
-		LOG_INFO << "Main thread, Message " << i;
-	}
+    list<shared_ptr<thread>> oThreads;
 
-	// wait for all the threads to finish
-	for (auto oThread : oThreads)
-	{
-		oThread->join();
-	}
+    // create 100 threads
+    for (int i = 0; i < 100; i++) {
+        oThreads.push_back(shared_ptr<thread>(new thread(threadFunc, i)));
+    }
 
-	return 0;
+    // also do logging from the main thread
+    for (int i = 0; i < 100; i++) {
+        // add a log message
+        LOG_INFO("Main thread, Message", i);
+    }
+
+    // wait for all the threads to finish
+    for (auto oThread : oThreads) {
+        oThread->join();
+    }
+
+    return 0;
 }
